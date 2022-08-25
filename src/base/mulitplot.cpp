695a5ba1165b25fee4ce3071981afb5bad76bf09
Stefan Hartinger <stefan_hartinger@gmx.de>
2022-08-25 14:59:45 +0200

    introduces class MultiPlot

---
+++ src/base/mulitplot.cpp
@@ -0,0 +1,8 @@
+#include "mulitplot.h"
+
+namespace Plotypus
+{
+    MulitPlot::MulitPlot(const std::string& title) :
+        Sheet(title)
+    {}
+}
---
+++ src/base/mulitplot.h
@@ -0,0 +1,15 @@
+#ifndef MULITPLOT_H
+#define MULITPLOT_H
+
+#include "sheet.h"
+
+namespace Plotypus
+{
+    class MulitPlot : public Sheet
+    {
+        public:
+            MulitPlot(const std::string& title);
+    };
+}
+
+#endif // MULITPLOT_H
