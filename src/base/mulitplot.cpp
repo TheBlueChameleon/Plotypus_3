c0dfacdd4547ead77612969cb7a2ee73a5dbd030
Stefan Hartinger <stefan_hartinger@gmx.de>
2022-09-09 17:34:18 +0200

    implements major parts of MultiPlot

--- src/base/mulitplot.cpp
+++ src/base/mulitplot.cpp
@@ -5,4 +5,326 @@ namespace Plotypus
     MulitPlot::MulitPlot(const std::string& title) :
         Sheet(SheetType::Multiplot, title), SheetsCollection(allowedSheetTypes)
     {}
+
+    // ====================================================================== //
+
+    MulitPlot& MulitPlot::reset()
+    {
+        Sheet::reset();
+
+        layout.reset();
+        blanks.clear();
+        options.reset();
+
+        frameSeparatorTxt   = "--------------------------------------------------------------------------------\n";
+
+        return *this;
+    }
+
+    MultiplotLayout_t MulitPlot::getLayout() const
+    {
+        return layout.value_or(DEFAULT_MULTIPLOT_LAYOUT);
+    }
+
+    MulitPlot& MulitPlot::setLayout(const MultiplotLayout_t& newLayout)
+    {
+        layout = newLayout;
+        return *this;
+    }
+
+    MulitPlot& MulitPlot::clearLayout()
+    {
+        layout.reset();
+        return *this;
+    }
+
+    GridPosition_t MulitPlot::getGridDimensions() const
+    {
+        if (layout.has_value())
+        {
+            return layout->gridDimensions;
+        }
+        else
+        {
+            return DEFAULT_GRID_POSITION;
+        }
+    }
+
+    MulitPlot& MulitPlot::setGridDimensions(const GridPosition_t& newGridDimensions)
+    {
+        if (layout.has_value())
+        {
+            layout->gridDimensions = newGridDimensions;
+        }
+        else
+        {
+            layout = {newGridDimensions};
+        }
+
+        return *this;
+    }
+
+    StackingOrder MulitPlot::getStackingOrder() const
+    {
+        if (layout.has_value())
+        {
+            return layout->stackingOrder;
+        }
+        else
+        {
+            return StackingOrder::Default;
+        }
+    }
+
+    MulitPlot& MulitPlot::setStackingOrder(const StackingOrder newStackingOrder)
+    {
+        if (layout.has_value())
+        {
+            layout->stackingOrder = newStackingOrder;
+        }
+        else
+        {
+            layout = {DEFAULT_GRID_POSITION};
+            layout->stackingOrder = newStackingOrder;
+        }
+
+        return *this;
+    }
+
+    StackingDirection MulitPlot::getStackingDirection() const
+    {
+        if (layout.has_value())
+        {
+            return layout->stackingDirection;
+        }
+        else
+        {
+            return StackingDirection::Default;
+        }
+    }
+
+    MulitPlot& MulitPlot::setStackingDirection(const StackingDirection newStackingDirection)
+    {
+        if (layout.has_value())
+        {
+            layout->stackingDirection = newStackingDirection;
+        }
+        else
+        {
+            layout = {DEFAULT_GRID_POSITION};
+            layout->stackingDirection = newStackingDirection;
+        }
+
+        return *this;
+    }
+
+    Margins_t MulitPlot::getMargins() const
+    {
+        if (layout.has_value())
+        {
+            return layout->margins.value_or(DEFAULT_MARGIN_SIZE);
+        }
+        else
+        {
+            return DEFAULT_MARGIN_SIZE;
+        }
+    }
+
+    MulitPlot& MulitPlot::setMargins(const Margins_t& newMargins)
+    {
+        if (layout.has_value())
+        {
+            layout->margins = newMargins;
+        }
+        else
+        {
+            layout = {DEFAULT_GRID_POSITION};
+            layout->margins = newMargins;
+        }
+
+        return *this;
+    }
+
+    MulitPlot& MulitPlot::clearMargins()
+    {
+        if (layout.has_value())
+        {
+            layout->margins.reset();
+        }
+
+        return *this;
+    }
+
+    MultiplotSpacing_t MulitPlot::getSpacing() const
+    {
+        if (layout.has_value())
+        {
+            return layout->spacing.value_or(DEFAULT_MULTIPLOT_SPACING);
+        }
+        else
+        {
+            return DEFAULT_MULTIPLOT_SPACING;
+        }
+    }
+
+    MulitPlot& MulitPlot::setSpacing(const MultiplotSpacing_t& newSpacing)
+    {
+        if (layout.has_value())
+        {
+            layout->spacing = newSpacing;
+        }
+        else
+        {
+            layout = {DEFAULT_GRID_POSITION};
+            layout->spacing = newSpacing;
+        }
+
+        return *this;
+    }
+
+    MulitPlot& MulitPlot::clearSpacing()
+    {
+        if (layout.has_value())
+        {
+            layout->spacing.reset();
+        }
+
+        return *this;
+    }
+
+    std::string MulitPlot::getOptions() const
+    {
+        return options.value_or("");
+    }
+
+    MulitPlot& MulitPlot::setOptions(const std::string& newOptions)
+    {
+        options = newOptions;
+        return *this;
+    }
+
+    MulitPlot& MulitPlot::clearOptions()
+    {
+        options.reset();
+        return *this;
+    }
+
+    const std::string& MulitPlot::getFrameSeparatorTxt() const
+    {
+        return frameSeparatorTxt;
+    }
+
+    void MulitPlot::setFrameSeparatorTxt(const std::string& newFrameSeparatorTxt)
+    {
+        frameSeparatorTxt = newFrameSeparatorTxt;
+    }
+
+    // ====================================================================== //
+    // writers
+
+    void MulitPlot::preprocessSheet(const std::string& autoFilenameBase, const std::string& extension) const
+    {
+        for (size_t i = 1u; auto sheet : sheets)
+        {
+            const std::string autoOutputFilename = autoFilenameBase + "_" + std::to_string(i);
+            sheet->preprocessSheet(autoOutputFilename, extension);
+            ++i;
+        }
+    }
+
+    void MulitPlot::writeTxtHead(std::ostream& hFile) const
+    {
+        Sheet::writeTxtHead(hFile);
+
+        // introduce multi section with some comment and title
+    }
+
+    void MulitPlot::writeTxtData(std::ostream& hFile) const
+    {
+        Sheet::writeTxtData(hFile);
+
+        for (size_t i = 1u; auto sheet : sheets)
+        {
+            sheet->writeTxtHead  (hFile);
+            sheet->writeTxtData  (hFile);
+            sheet->writeTxtOverlays(hFile);
+
+            if (i != sheets.size())
+            {
+                hFile << frameSeparatorTxt;
+            }
+            ++i;
+        }
+    }
+
+    void MulitPlot::writeDatData() const
+    {
+        for (auto sheet : sheets)
+        {
+            sheet->writeDatData();
+        }
+    }
+
+    void MulitPlot::writeScriptHead(std::ostream& hFile) const
+    {
+        // explicitly no call to parent due to different handling of title
+
+        if (customScriptBegin.has_value())
+        {
+            hFile << "# " << std::string(76, '-') << " #\n";
+            hFile << "# custom setup script I" << std::endl << std::endl;
+            hFile << customScriptBegin.value() << std::endl;
+            hFile << std::endl;
+        }
+
+        hFile << "# " << std::string(76, '-') << " #\n";
+        hFile << "# generated setup script" << std::endl << std::endl;
+
+        if (defaultFont.has_value())
+        {
+            hFile << "set font " << std::quoted(defaultFont.value()) << std::endl;
+        }
+
+        // set multiplot
+        // title, titleFont aka font
+
+//        if (title.has_value())
+//        {
+//            hFile << "set title " << std::quoted("{/" + titleFont.value_or("") + " " + title.value() + "}") << std::endl;
+//        }
+
+        // layout
+        // options
+
+    }
+
+    void MulitPlot::writeScriptData(std::ostream& hFile, const StylesCollection& stylesCollection) const
+    {
+        Sheet::writeScriptData(hFile, stylesCollection);
+
+        for (size_t i = 1u; auto sheet : sheets)
+        {
+            hFile << "# " << std::string(76, '=') << " #\n";
+            hFile << "# subplot " << i << std::endl << std::endl;
+
+            // maybe: move writeCleanSheetCommands to util?
+//            if (needCleanSheetCommands && sheet->getType() == SheetType::Sheet) {needCleanSheetCommands = false; writeCleanSheetCommands(hFile);}
+//            else if                      (sheet->getType() != SheetType::Sheet) {needCleanSheetCommands = true ;}
+
+            sheet->writeScriptHead      (hFile);
+            sheet->writeScriptOverlays  (hFile);
+            sheet->writeScriptData      (hFile, stylesCollection);
+            sheet->writeScriptFooter    (hFile, i);
+            ++i;
+        }
+    }
+
+    void MulitPlot::writeScriptFooter(std::ostream& hFile, const int pageNum) const
+    {
+        // explicityl call parent at END of method
+
+        // unset multiplot
+
+        Sheet::writeScriptFooter(hFile, pageNum);
+    }
 }
--- src/base/mulitplot.h
+++ src/base/mulitplot.h
@@ -1,10 +1,20 @@
 #ifndef MULITPLOT_H
 #define MULITPLOT_H
 
+#include <optional>
+#include <set>
+#include <string>
+
+#include "../definitions/types.h"
 #include "sheet.h"
 
 namespace Plotypus
 {
+
+    //! @todo add size/origin on Sheet
+    //! @todo add convenience non-uniform grid creators aka arrangeX
+    //! @todo lock adding overlays (as positioning cannot be distinguished from in-frame); maybe add boolean 'locked' to OverlaysCollection
+
     class MulitPlot : public Sheet, public SheetsCollection
     {
         protected:
@@ -12,8 +22,62 @@ namespace Plotypus
                                                                                    SheetType::PlotOrthogonalAxis,
                                                                                    SheetType::PlotRadialAxes
                                                                                   };
+
+            // note: optional string 'title' present from Sheet
+
+            std::optional<MultiplotLayout_t>    layout;
+            std::set<size_t>                    blanks; // triggers "set multiplot next"
+
+            std::optional<std::string>          options;
+
+            std::string                         frameSeparatorTxt   = "--------------------------------------------------------------------------------\n";
+
         public:
             MulitPlot(const std::string& title);
+
+            MulitPlot&          reset();
+
+            MultiplotLayout_t   getLayout() const;
+            MulitPlot&          setLayout(const MultiplotLayout_t& newLayout);
+            MulitPlot&          clearLayout();
+
+            GridPosition_t      getGridDimensions() const;
+            MulitPlot&          setGridDimensions(const GridPosition_t& newGridDimensions);
+
+            StackingOrder       getStackingOrder() const;
+            MulitPlot&          setStackingOrder(const StackingOrder newStackingOrder);
+
+            StackingDirection   getStackingDirection() const;
+            MulitPlot&          setStackingDirection(const StackingDirection newStackingDirection);
+
+            Margins_t           getMargins() const;
+            MulitPlot&          setMargins(const Margins_t& newMargins);
+            MulitPlot&          clearMargins();
+
+            MultiplotSpacing_t  getSpacing() const;
+            MulitPlot&          setSpacing(const MultiplotSpacing_t& newSpacing);
+            MulitPlot&          clearSpacing();
+
+            std::string         getOptions() const;
+            MulitPlot&          setOptions(const std::string& newOptions);
+            MulitPlot&          clearOptions();
+
+            const std::string&  getFrameSeparatorTxt() const;
+            void                setFrameSeparatorTxt(const std::string& newFrameSeparatorTxt);
+
+            // -------------------------------------------------------------- //
+            // writers
+
+            virtual void preprocessSheet(const std::string& autoFilenameBase, const std::string& extension) const;
+
+            virtual void writeTxtHead       (std::ostream& hFile) const;
+            virtual void writeTxtData       (std::ostream& hFile) const;
+
+            virtual void writeDatData() const;
+
+            virtual void writeScriptHead    (std::ostream& hFile) const;
+            virtual void writeScriptData    (std::ostream& hFile, const StylesCollection& stylesCollection) const;
+            virtual void writeScriptFooter  (std::ostream& hFile, const int pageNum) const;
     };
 }
 
